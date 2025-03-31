300cfbfcc09e8d3bc0626bf1a5a92b5e185a46c0
Harshil Patwa <harshil@mavenbird.com>
2025-03-31 18:04:47 +0000

    Fix database query error on occur on product edit page - KJ

--- Ui/DataProvider/Product/EstimatedProfitField.php
+++ Ui/DataProvider/Product/EstimatedProfitField.php
@@ -1,27 +1,46 @@
 <?php 
 namespace Mavenbird\EstimatedProfit\Ui\DataProvider\Product;
+ 
+use Magento\Framework\App\ResourceConnection;
+ 
 class EstimatedProfitField implements \Magento\Ui\DataProvider\AddFieldToCollectionInterface 
 { 
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+ 
+    /**
+     * @param ResourceConnection $resourceConnection
+     */
+    public function __construct(
+        ResourceConnection $resourceConnection
+    ) {
+        $this->resourceConnection = $resourceConnection;
+    }
+ 
     public function addField(
         \Magento\Framework\Data\Collection $collection,
         $field,
         $alias = null
     ) 
     { 
+        $connection = $this->resourceConnection->getConnection();
+        $productDecimalTable = $this->resourceConnection->getTableName('catalog_product_entity_decimal');
+ 
         $collection->joinField(
              "price", 
-             "catalog_product_entity_decimal", 
+             $productDecimalTable, 
              "value",
              "entity_id = entity_id",
              "at_price.attribute_id = 77",
              "left" 
         );
-
+ 
         $collection->getSelect()->joinLeft(
-            array('at_estimated_profit'=>'catalog_product_entity_decimal'),
+            ['at_estimated_profit' => $productDecimalTable],
             'at_estimated_profit.`entity_id` = e.`entity_id` AND at_estimated_profit.`attribute_id`=81',
-            array('ROUND(((at_price.`value` - at_estimated_profit.`value`)*at_qty.`qty`),2) AS estimated_profit')
+            ['ROUND(((at_price.`value` - at_estimated_profit.`value`)*at_qty.`qty`),2) AS estimated_profit']
         );
-
     }
 }
